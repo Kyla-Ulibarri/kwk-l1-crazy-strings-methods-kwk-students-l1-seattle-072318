09995132458696eee50da5577bedb51a4b25b16a
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-25 19:55:47 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,7 +2,7 @@
 
 first_string= "First"
 second_string= "sEconD"
-def crazy_strings(first_string, second_string)
+def crazy_strings(first_string="First", second_string)
   puts "#{first_string}".reverse.upcase
   puts "#{second_string}".swapcase.gsub("S", "Z")
 end 
